b43bd5b1b5ab49b051ed581cae8c9bf150ee115e
Zack Adlington <zackads@users.noreply.github.com>
2024-02-25 10:32:08 +0000

    Define inner product

--- src/Vector.java
+++ src/Vector.java
@@ -8,11 +8,32 @@ public class Vector {
         this.components = new ArrayList<>(Arrays.asList(c));
     }
 
+    public static ComplexNumber innerProduct(Vector v1, Vector v2) {
+        if (v1.dimension() != v2.dimension()) throw new IllegalArgumentException("Vectors must have the same number of components");
+
+        var dotProd = ComplexNumber.ZERO;
+
+        for (int i = 0; i < v1.dimension(); i++) {
+            dotProd = ComplexNumber.sum(
+                    dotProd,
+                    ComplexNumber.product(
+                            v1.components.get(i).conjugate(),
+                            v2.components.get(i)
+                    ));
+        }
+
+        return dotProd;
+    }
+
     public double norm() {
         return Math.sqrt(
                 components.stream()
                         .map(z -> ComplexNumber.product(z, z.conjugate()))
                         .mapToDouble(ComplexNumber::realPart)
                         .sum());
+    }
+
+    public double dimension() {
+        return components.size();
     }
 }
--- src/VectorTest.java
+++ src/VectorTest.java
@@ -1,8 +1,37 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Nested;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class VectorTest {
 
+    @Nested
+    class InnerProduct {
+        @Test
+        void givenTwoVectorsOfDifferentDimension_thenThrowException() {
+            var v1 = new Vector(ComplexNumber.ZERO, ComplexNumber.ZERO);
+            var v2 = new Vector(ComplexNumber.ZERO);
+
+            assertThrows(IllegalArgumentException.class, () -> Vector.innerProduct(v1, v2));
+        }
+
+        @Test
+        void givenTwoRealVectors_thenReturnTheirDotProduct() {
+            var v1 = new Vector(ComplexNumber.rectangular(2, 0), ComplexNumber.rectangular(6, 0));
+            var v2 = new Vector(ComplexNumber.rectangular(4, 0), ComplexNumber.rectangular(1, 0));
+
+            assertEquals(ComplexNumber.rectangular(14, 0), Vector.innerProduct(v1, v2));
+        }
+
+        @Test
+        void givenTwoComplexVectors_thenReturnTheirStandardComplexInnerProduct() {
+            var v1 = new Vector(ComplexNumber.rectangular(2, 1), ComplexNumber.rectangular(6, 3));
+            var v2 = new Vector(ComplexNumber.rectangular(4, 2), ComplexNumber.rectangular(1, 7));
+
+            assertEquals(ComplexNumber.rectangular(37, 39), Vector.innerProduct(v1, v2));
+        }
+    }
     @Test
     void norm() {
         var v1 = new Vector(
