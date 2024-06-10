fd8b9fa6eb3137abc3d2a5a9280929c798af8513
Kasim Rafique <kasim@rafique.co.uk>
2024-06-10 14:07:06 +0100

    add javadocs to complexnumber.java
    ensure equals and hashcode methods in each class
    
    Signed-off-by: Kasim Rafique <kasim@rafique.co.uk>

--- src/main/java/LA/ComplexNumber.java
+++ src/main/java/LA/ComplexNumber.java
@@ -1,12 +1,15 @@
 package LA;
 
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.Optional;
 
+/**
+ * A class representing a complex number. One where the square root of a negative is defined.
+ */
 public class ComplexNumber {
     private final double a;
     private final double bi;
-
     public static final ComplexNumber MULTIPLICATIVE_IDENTITY = new ComplexNumber(1, 0);
     public static final ComplexNumber ZERO = new ComplexNumber(0, 0);
 
@@ -17,18 +20,44 @@ public class ComplexNumber {
         this.bi = bi;
     }
 
+    /**
+     * Create a complex number with only a real element.
+     *
+     * @param x The real component
+     * @return The new complex number
+     */
     public static ComplexNumber real(double x) {
         return new ComplexNumber(x, 0);
     }
 
+    /**
+     * Create a complex number with a real and imaginary part.
+     *
+     * @param a The real part
+     * @param bi The imaginary part
+     * @return The new complex number
+     */
     public static ComplexNumber rectangular(double a, double bi) {
         return new ComplexNumber(a, bi);
     }
 
+    /**
+     * Create a complex number from a polar co-ordinate.
+     *
+     * @param modulus The modulus
+     * @param argument The argument
+     * @return The new complex number
+     */
     public static ComplexNumber polar(double modulus, double argument) {
        return new ComplexNumber(modulus * Math.cos(argument), modulus * Math.sin(argument));
     }
 
+    /**
+     * Calculate the sum of complex numbers.
+     *
+     * @param zs The complex numbers to sum together
+     * @return The sum as a complex number
+     */
     public static ComplexNumber sum(ComplexNumber... zs) {
         return Arrays.stream(zs).reduce(
                 ComplexNumber.ADDITIVE_IDENTITY,
@@ -36,6 +65,12 @@ public class ComplexNumber {
         );
     }
 
+    /**
+     * Calculate the product of complex numbers.
+     *
+     * @param zs The complex numbers to multiply together
+     * @return The product as a complex number
+     */
     public static ComplexNumber product(ComplexNumber... zs) {
         return Arrays.stream(zs).reduce(
                 ComplexNumber.MULTIPLICATIVE_IDENTITY,
@@ -43,26 +78,41 @@ public class ComplexNumber {
         );
     }
 
+    /**
+     * @return Real component
+     */
     public double realPart() {
         return this.a;
     }
 
+    /**
+     * @return Imaginary part
+     */
     public double imaginaryPart() {
         return this.bi;
     }
 
+    /**
+     * @return The complex conjugate of a complex number
+     */
     public ComplexNumber conjugate() {
         return new ComplexNumber(a, -bi);
     }
 
+    /**
+     * @return The modulus / norm of the complex number
+     */
     public double modulus() {
         return Math.sqrt(a * a + bi * bi);
     }
 
 
-    // Returns the positive root of the complex number
-    //        sqrt of imaginary number is imaginary
-    //        https://www.cuemath.com/algebra/square-root-of-complex-number/
+    /**
+     * Calculates the square root of the complex number. The square root of an imaginary number is imaginary.
+     * @see <a href="https://www.cuemath.com/algebra/square-root-of-complex-number/">Source</a>
+     *
+     * @return The positive root of the complex number
+     */
     public ComplexNumber sqrt(){
         // Handle all cases where b is 0
         if ((bi == 0) && (a>=0)) return ComplexNumber.rectangular(Math.sqrt(a),0);
@@ -72,8 +122,11 @@ public class ComplexNumber {
                 (bi/Math.abs(bi))*Math.sqrt((this.modulus() - a)/2.0));
     }
 
-    // Argument of complex number between 0 - 2Pi
-    // i.e. not negative
+    /**
+     * Calculates the argument of a complex number.
+     *
+     * @return The argument between 0 and 2π
+     */
     public Double argument() {
         double r = 0.0;
         if (a == 0 && bi == 0) r = 0.0;
@@ -90,10 +143,18 @@ public class ComplexNumber {
         return r;
     }
 
+    /**
+     * @return The additive inverse for the complex number
+     */
     public ComplexNumber additiveInverse(){
         return ComplexNumber.rectangular(this.a*-1, this.bi*-1);
     }
 
+    /**
+     * Calculates the multiplicative inverse for the complex number. Note that {@link #ZERO} does not have an inverse.
+     *
+     * @return Empty if {@link #ZERO} or an optional of the multiplicative inverse
+     */
     public Optional<ComplexNumber> multiplicativeInverse(){
         if (a == 0 && bi == 0) return Optional.empty();
 
@@ -111,5 +172,10 @@ public class ComplexNumber {
     @Override
     public String toString() {
         return "ComplexNumber{" + a + " + " + bi + "i}";
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(a, bi);
     }
 }
